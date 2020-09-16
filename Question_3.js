fcf3579ee9551af0df723b27c22397e47794d178
spraveensri <53633834+spraveensri@users.noreply.github.com>
2020-09-15 23:52:43 -0700

    Day 4 Assignment

---
+++ Question_3.js
@@ -0,0 +1,23 @@
+let info = [ {
+    name: "Arun",
+    age: 23,
+    country: "India",
+    hobbies: ["playing football", "Watching Tv"]
+ },
+ {
+     name: "Jack",
+    age: 23,
+    country: "America",
+    hobbies: ["playing football", "video game"]
+ },
+ {
+     name: "Kumar",
+    age: 27,
+    country: "India",
+    hobbies: ["Drawing", "reading"]},
+ ];
+ 
+ for (let i = 0; i < info.length; i++) {
+    console.log(info[i]);
+    
+ }
