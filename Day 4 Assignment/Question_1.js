b042f24fdf84bd6af5aa3aedb0a86f5d2d98413a
spraveensri <53633834+spraveensri@users.noreply.github.com>
2020-09-16 00:10:40 -0700

    Day 4 Assignment

---
+++ Day 4 Assignment/Question_1.js
@@ -0,0 +1,22 @@
+function Image1() {
+    const ele = document.getElementById("image");
+    console.log(ele.id);
+    const newUrl = "https://terrigen-cdn-dev.marvel.com/content/prod/1x/theavengers_lob_crd_03.jpg";
+  
+    ele.src = newUrl;
+  }
+  function Image2() {
+    const ele = document.getElementById("image");
+    console.log(ele.id);
+    const newUrl = "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg";
+  
+    ele.src = newUrl;
+  }
+  function Image3() {
+    const ele = document.getElementById("image");
+    console.log(ele.id);
+    const newUrl = "https://lh3.googleusercontent.com/XDg-bt655am_Q-7X-I0s64Kq8SJKfb7BBTHkUVbFR6-zDNv9J7rW61xZn0BB3SVCJ6gz";
+  
+    ele.src = newUrl;
+  }
+ 
---
+++ Day 4 Assignment/Question_2.js
@@ -0,0 +1,7 @@
+function copyinput(){
+
+  let nameinput=document.getElementsByClassName('input');
+
+   nameinput[1].value=nameinput[0].value;
+
+ }    
